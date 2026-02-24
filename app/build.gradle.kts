43eb7f9e963ec04653d38ce7302e997839c320e4
Anabia Shelia <shelia@Anabias-MacBook-Pro.local>
2026-02-25 00:37:00 +0530

    fixed bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material)
+    implementation(libs.androidx.compose.material.icons.extended)
     
     // Navigation Setup
     implementation(libs.navigation.compose)
